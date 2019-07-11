869b8404ed09001d3e2666776cdc1e69232d3b82
Hesam Eskandari <eskandarihesam@gmail.com>
2019-07-11 01:15:03 -0400

    New version
    Eraser Added, perfomance improved, true size of the photo is now preserved.

--- ColorPop.py
+++ ColorPop.py
@@ -2,12 +2,13 @@
 #https://www.instagram.com/eskandarihesam
 #https://github.com/Hesam-Eskandari
 #https://www.facebook.com/EskandariHesam
-
+#keyboard keys: 1,2,3,4,5,6,7,8,9 for size of the pen or eraser and 'r' as reverse mode for the eraser
+#go to line 90 to change the photo's name to load (default name is 'photo.jpg')
 
 from cv2 import EVENT_LBUTTONUP, imshow, imread, waitKey, destroyAllWindows, COLOR_BGR2GRAY
 from cv2 import cvtColor, namedWindow, setMouseCallback, COLOR_GRAY2BGR, COLOR_BGR2HSV
 from cv2 import COLOR_HSV2BGR, EVENT_LBUTTONDOWN, EVENT_MOUSEMOVE, imwrite, resize
-from numpy import shape
+from numpy import shape, append, array, sum
 
 def click(event,x,y,flags,param):
     """
@@ -23,18 +24,22 @@ def click(event,x,y,flags,param):
         x_c,y_c,continum = x,y,1
     else:
         x_c,y_c = 0,0
-def color(ground,gr_pop,x_c,y_c):
+def color(ground,gr_pop,x_c,y_c,original,reverse):
     """
     Input:(original_image,under_live_process_image,cursor_x,cursor_y)
     Output: processed_gr_pop
     """
-    gr_hsv = cvtColor(ground,COLOR_BGR2HSV)
+    if reverse == 1:
+        gray_subs =  cvtColor(ground.copy(),COLOR_BGR2GRAY)
+        ground_c = cvtColor(gray_subs,COLOR_GRAY2BGR)
+    else:
+        ground_c = ground.copy()
+    gr_hsv = cvtColor(ground_c,COLOR_BGR2HSV)
     global coords
-    #coords = np.aray([[x_c,y_c]])
     coords = [[x_c,y_c]]
-    #a = 15
     gr_pop_hsv = cvtColor(gr_pop,COLOR_BGR2HSV)
-    #def tail(ground,x,y,a,i=0):
+
+        
     def tail(gr_hsv,x,y,a,i=0):
         """
         Input: (original_image,examin_x,examin_y,step_size)
@@ -46,75 +51,104 @@ def color(ground,gr_pop,x_c,y_c):
             for element in coords:
                 if element == [item[0],item[1]]:
                     allow = 0
-                    #print('here_allow')
             if allow == 1:
-                if item[1]<shape(gr_hsv)[0] and item[0]<shape(gr_hsv)[1]:
-                #if item[1]<shape(ground)[0] and item[0]<shape(ground)[1]:
+
+                if abs(item[1])<shape(gr_hsv)[0] and abs(item[0])<shape(gr_hsv)[1]:
                     if gr_pop_hsv[item[1],item[0],0] == 0:
-                        #if abs(ground[item[1],item[0],0]-ground[y,x,0]) < thresh1:
                         if abs(gr_hsv[item[1],item[0],0]-gr_hsv[y,x,0]) < thresh1:
-                            #if abs(ground[item[1],item[0],1]-ground[y,x,1]) < thresh2:
                             if abs(gr_hsv[item[1],item[0],1]-gr_hsv[y,x,1]) < thresh2:
-                                #if abs(ground[item[1],item[0],2]-ground[y,x,2]) < thresh3:
                                 if abs(gr_hsv[item[1],item[0],2]-gr_hsv[y,x,2]) < thresh3:
-                                    #coords = np.append(coords,[[item[0],item[1]]],axis=0)
                                     coords.append([item[0],item[1]])
-                                    #print(i)
                                     if i >= 1000:
                                         break
                                     else:
-                                        #tail(ground,item[0],item[1],a,i+1)
                                         tail(gr_hsv,item[0],item[1],a,i+1)
+
+
+                                
         click(0,0,0,0,0)
-        #print('i=',i)
         return coords
-    
-    #coords = tail(ground,x_c,y_c,a,0)
-    coords = tail(gr_hsv,x_c,y_c,a,0)
-    #print(coords)                   
-        
-    #h,s,v = gr_hsv[y_c,x_c,:]
+    if not reverse:
+        coords = tail(gr_hsv,x_c,y_c,a,0)
+    else:
+        coords = [[x_c,y_c]]                  
     
     for item in coords:
         x,y = item[0],item[1]
-        m_y,M_y = max(y-a+1,0),min(y+a,shape(ground)[0])
-        m_x,M_x = max(x-a+1,0),min(x+a,shape(ground)[1])
-        #gr_pop_hsv[m_y:M_y,m_x:M_x,:] = gr_hsv[m_y:M_y,m_x:M_x,:].copy()
-        #gr_pop = cvtColor(gr_pop_hsv,COLOR_HSV2BGR)
-        gr_pop[m_y:M_y,m_x:M_x,:] = ground[m_y:M_y,m_x:M_x,:].copy()
-    return gr_pop
+        m_y,M_y = max(y-a+1,0),min(y+a,shape(ground_c)[0])
+        m_x,M_x = max(x-a+1,0),min(x+a,shape(ground_c)[1])
+        gr_pop[m_y:M_y,m_x:M_x,:] = ground_c[m_y:M_y,m_x:M_x,:].copy()
+        original = append(original,[[m_y,M_y,m_x,M_x,reverse]],axis=0)
+    return gr_pop, original
 global continum, a
+original = array([[0,0,0,0,0]])
 a,mode = 4,1
 continum = 0
 x_c,y_c = 0,0
 namedWindow('desk')
 setMouseCallback('desk',click)
-#ground = np.ones((300,600,3),'uint8')
-ground = imread('photo5.jpg')
-ground = resize(ground,(int(650*shape(ground)[1]/shape(ground)[0]),650))
+ground2 = imread('photo.jpg')
+coeff = 1000
+ground = resize(ground2,(int(coeff*shape(ground2)[1]/float(shape(ground2)[0])+0.5),int(coeff+0.5)))
 gr_pop = cvtColor(ground,COLOR_BGR2GRAY)
 gr_pop = cvtColor(gr_pop,COLOR_GRAY2BGR)
-#gr_pop = 255*np.ones((300,600,3),'uint8')
+reverse = 0
 while True:
     imshow('desk',gr_pop)
-    key = waitKey(40*mode) & 0xFF
+    key = waitKey(1*mode) & 0xFF
     if key == 27:
         break
     elif key == 49:
-        a,mode = 3,1
+        a,mode = 2,1
     elif key == 50:
-        a,mode = 4,1
+        a,mode = 3,1
     elif key == 51:
-        a,mode = 6,2
+        a,mode = 4,1
     elif key == 52:
-        a,mode = 8,3
+        a,mode = 6,2
     elif key == 53:
-        a,mode = 12,5
+        a,mode = 9,3
     elif key == 54:
+        a,mode = 12,4
+    elif key == 55:
         a,mode = 15,5
+    elif key == 56:
+        a,mode = 18,6
+    elif key == 57:
+        a,mode = 22,7
+    elif key == ord('r'):
+        reverse = 1-reverse
+        a = 4
+        key = 255
     if x_c != 0:
-        gr_pop = color(ground,gr_pop,x_c,y_c)
-        #print(np.shape(gr_pop))
+        gr_pop, original = color(ground.copy(),gr_pop,x_c,y_c,original,reverse)
         click(0,0,0,0,0)
 destroyAllWindows()
-imwrite('color_pop.jpg',gr_pop)
+original = original[1:,:]*shape(ground2)[0]/float(shape(ground)[0])+0.5
+original = original.astype('uint16')
+gray_final = cvtColor(ground2,COLOR_BGR2GRAY)
+color_pop_final = cvtColor(gray_final,COLOR_GRAY2BGR)
+gray = color_pop_final.copy()
+for row in original:
+    if not row[-1]:
+        color_pop_final[row[0]:row[1],row[2]:row[3],:]=ground2[row[0]:row[1],row[2]:row[3],:]
+    else:
+        color_pop_final[row[0]:row[1],row[2]:row[3],:]=gray[row[0]:row[1],row[2]:row[3],:]
+imwrite('color_pop.jpg',color_pop_final)
+
+
+# In[31]:
+
+
+x = np.array([[1,2]])
+x = np.append(x,[[2,3]],axis=0)
+x = np.append(x,[[2,4]],axis=0)
+print(x)
+sum(np.isin(x,[1,2]),0)
+
+
+# In[34]:
+
+
+
+
